c38a29c0cecb8f0cbad181d9d463f1c7c86a63ea
Peter Winton <pwinton@indigobio.com>
2022-01-11 22:54:13 -0500

    eval_ast

--- lib/rds.rb
+++ lib/rds.rb
@@ -17,8 +17,18 @@ end
 
 def block_ast(p, full: false)
   file, beg_line, beg_col = p.source_region
-  ast = AstCache.get(file)
-  node = find_block(ast, beg_line, beg_col)
+  node =
+  if $eval_ast
+    # beg_line and beg_col are relative to $eval_str
+    eval_ast2 = Parser::CurrentRuby.parse($eval_str)
+    inner = find_block_line_col(eval_ast2, beg_line, beg_col)
+    # use the offset of $eval_ast and eval_ast2 to find the original ast with true location info corresponding to inner
+    abs_begin_pos = $eval_ast.location.expression.begin_pos + inner.location.expression.begin_pos
+    find_block_offset($eval_ast, abs_begin_pos)
+  else
+    ast = AstCache.get(file)
+    find_block_line_col(ast, beg_line, beg_col)
+  end
   if node.nil?
     nil
   elsif full
@@ -28,16 +38,45 @@ def block_ast(p, full: false)
   end
 end
 
-def find_block(node, beg_line, beg_col)
+def find_block_line_col(node, beg_line, beg_col)
   if node.is_a?(Parser::AST::Node)
     if node.type == :block && node.location.begin.line == beg_line && node.location.begin.column == beg_col
       return node
     else
       node.children.each do |c|
-        found = find_block(c, beg_line, beg_col)
+        found = find_block_line_col(c, beg_line, beg_col)
         return found if found
       end
     end
   end
   nil
+end
+
+def find_block_offset(node, offset)
+  if node.is_a?(Parser::AST::Node)
+    if node.type == :block && node.location.expression.begin_pos == offset
+      return node
+    else
+      node.children.each do |c|
+        found = find_block_offset(c, offset)
+        return found if found
+      end
+    end
+  end
+  nil
+end
+
+def eval_ast(ast, binding)
+  old_eval_ast = $eval_ast
+  old_eval_str = $old_eval_str
+  $eval_ast = ast
+  $eval_str = ast_text(ast)
+  eval($eval_str, binding)
+ensure
+  $eval_ast = old_eval_ast
+  $eval_str = old_eval_str
+end
+
+def ast_text(node)
+  node.location.expression.source
 end
--- spec/rds_spec.rb
+++ spec/rds_spec.rb
@@ -13,9 +13,22 @@ RSpec.describe Rds do
 
     expect(ast_text(block_ast(proc { x + y }))).to eql "x + y"
     expect(ast_text(block_ast(proc { x + y }, full: true))).to eql "proc { x + y }"
-  end
-end
 
-def ast_text(node)
-  node.location.expression.source
+    a = block_ast(proc { x + y }, full: true)
+    b = Parser::CurrentRuby.parse(ast_text(a), a.location.begin.source_buffer.name)
+    [a,b]
+  end
+  it "eval_ast" do
+    x = 2
+    y = 3
+    expect(eval_ast(block_ast(proc { x + y }), binding)).to eql 5
+
+    inner = eval_ast(block_ast(proc { block_ast(proc { a + b }) }), binding)
+    expect(ast_text(inner)).to eql "a + b"
+
+    inner = eval_ast(block_ast(proc { block_ast(proc { a + b }, full: true) }), binding)
+    expect(ast_text(inner)).to eql "proc { a + b }"
+    expect(inner.location.expression.source_buffer.name).to eql __FILE__
+    expect(inner.location.begin.line).to eql __LINE__ - 3
+  end
 end
