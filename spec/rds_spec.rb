fc998bcbcab85b8afd7fbfbfbc2af9b81f5b771e
Peter Winton <pwinton@indigobio.com>
2022-01-11 22:40:21 -0500

    ast_text

--- spec/rds_spec.rb
+++ spec/rds_spec.rb
@@ -3,7 +3,7 @@
 # - syntax
 # - quasisyntax / unsyntax
 # - define_syntax: transform ast and eval
-# - eval(ast, binding) -> value
+# - eval_ast(ast, binding) -> value
 # - block_ast(block)
 
 RSpec.describe Rds do
@@ -11,11 +11,11 @@ RSpec.describe Rds do
     # result = proc { 5 }.source_region
     # puts result.inspect
 
-    expect(unp(block_ast(proc { x + y }))).to eql "x + y"
-    expect(unp(block_ast(proc { x + y }, full: true))).to eql "proc { x + y }"
+    expect(ast_text(block_ast(proc { x + y }))).to eql "x + y"
+    expect(ast_text(block_ast(proc { x + y }, full: true))).to eql "proc { x + y }"
   end
 end
 
-def unp(x)
-  Unparser.unparse(x).gsub(/\n\s*/, " ")
+def ast_text(node)
+  node.location.expression.source
 end
