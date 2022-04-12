5252e66df8077225e047f4849f3e24557f440e16
Peter Winton <pwinton@indigobio.com>
2022-04-11 20:26:05 -0400

    make_calibration2

--- spec/rdm1.rb.m
+++ spec/rdm1.rb.m
@@ -1,11 +1,21 @@
-defmacro(:make_calibration) do |k, args|
+defmacro(:make_calibration1) do |k, args|
   quasisyntax { [:calibration, unsyntax_splicing(args)] }
 end
 
-class Rdm1
-  def self.make_a_calibration
-    cal = make_calibration(2, :none, :ignore)
-    cal
-  end
+defmacro(:make_calibration2) do |k, args|
+  args[0] => [:kwargs, *pairs]
+  array_args = pairs.map do |arg|
+    arg => [:pair, [:sym, field], value]
+    [field, value]
+  end.to_h.values_at(:degree, :weighting, :origin)
+  quasisyntax { [:calibration, unsyntax_splicing(array_args)] }
+end
+
+def make_a_calibration1
+  make_calibration1(2, "none", "ignore")
+end
+
+def make_a_calibration2
+  make_calibration2(degree: 2, weighting: "none", origin: "ignore")
 end
 
--- spec/rdm_spec.rb
+++ spec/rdm_spec.rb
@@ -6,6 +6,7 @@ require_relative_expand("rdm1")
 
 RSpec.describe Rdm do
   it "works" do
-    expect(Rdm1.make_a_calibration).to eql [:calibration, 2, :none, :ignore]
+    expect(make_a_calibration1).to eql [:calibration, 2, "none", "ignore"]
+    expect(make_a_calibration2).to eql [:calibration, 2, "none", "ignore"]
   end
 end
